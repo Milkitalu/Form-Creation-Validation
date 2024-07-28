af4d19923f58f5ab0e94d64623dfa68e151bcac6
Talaku Getahun <getahuntalaku@gmail.com>
2024-07-28 17:43:55 +0300

    After corecting updating my code

--- script.js
+++ script.js
@@ -17,32 +17,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (username.length < 3) {
         isValid = false;
-        messages.push() = "Username should be greater than three characters";
+        messages.push("Username should be greater than three characters");
     }
 
     if (email.includes('@') && email.includes('.')) {
         console.log("Valid Email")
     } else {
         isValid = false;
-        messages.push() = "Please enter valid email";
+        messages.push("Please enter valid email");
     }
 
     if (password.length >= 8) {
         console.log("Your password matches minimum character");
     } else {
         isValid=false;
-        messages.push() = "Password should be at least eight character";
+        messages.push("Password should be at least eight character");
     }
 
-    messages["message"].feedbackDiv.style.display = block ;
+    feedbackDiv.style.display = block ;
 
     if (isValid === true) {
         feedbackDiv.textContent = "Registration successful!";
         feedbackDiv.style.color = "#28a745";
 
     } else {
-        feedbackDiv.innerHTML = message.join("<br>");
-        feedbackDiv.style.color = "#dc3545" ;
+        messages.forEach(message => {
+            feedbackDiv.innerHTML = messages.join("<br>");
+            feedbackDiv.style.color = "#dc3545" ;          
+        });
+
     }
 
 });
